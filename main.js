66ad131d1b6b0f0ee328473d9d2e898dd68e42dd
DeftSlyNinja <christopher_masson@hotmail.com>
2024-01-24 19:29:22 -0800

    Constructor and array created

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
+let Food = function(pName, pCalories){
+    this.name = pName;
+    this.calories = parseInt(pCalories);
+}
 let foodArray = [];
+foodArray.push(new Food('Apple', 95));
+foodArray.push(new Food('Orange', 110));
+
+let output = document.getElementById("totalDisplay").innerHTML
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
-
+    document.getElementById("subButton").addEventListener('click', function(event){
+        //placeholder
+        return null;
+    })
 });
